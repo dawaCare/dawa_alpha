08917134cb1d9273f2946afe8d93d8fdac2f461c
dta613 <dta613@gmail.com>
2017-06-30 08:16:16 -0700

    Add files

---
+++ dashboard/admin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from models import Patient
+
+# Register your models here.
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('Patient_name', 'age', 'location', 'condition', 'medication', 'initial_visit', 'followup_appt', 'reminder_freq')
+
+admin.site.register(Patient, PatientAdmin)
---
+++ dashboard/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.db.models import fields
+
+# Create your models here.
+class Patient(models.Model):
+    Patient_name = models.CharField(max_length=60)
+    age = models.PositiveSmallIntegerField()
+    location = models.CharField(max_length=30)
+    condition = models.CharField(max_length=150)
+    medication = models.CharField(max_length=30)
+    initial_visit = models.DateField()
+    followup_appt = models.DateField()
+    reminder_freq = models.PositiveSmallIntegerField()
